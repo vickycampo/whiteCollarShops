9d587d11e683ba3661a716b221d8b5addf45162d
Ana Virginia Campo Lemus <vickycampo@gmail.com>
2019-10-27 02:08:18 +0200

    20191027-02:06 - Adding CSS files in localhost

--- whiteCollarShops/src/main/java/com/whiteCollar/shops/configuration/MvcConfig.java
+++ whiteCollarShops/src/main/java/com/whiteCollar/shops/configuration/MvcConfig.java
@@ -1,16 +1,18 @@
 package com.whiteCollar.shops.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer
+{
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
     }
+
 }
